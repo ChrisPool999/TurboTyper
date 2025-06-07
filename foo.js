380f32c798496e34582155d90cabdedac34ffa65
Chris Pool <Brisbool999@gmail.com>
2025-06-07 19:13:34 +0800

    refactored into single class

--- foo.js
+++ foo.js
@@ -1,13 +1,3 @@
-async function loadQuotes() {
-  try {
-    const res = await fetch('quotes.json');
-    const data = await res.json();
-    return data;
-  } catch (err) {
-    console.error(err);
-  }
-}
-
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -17,45 +7,79 @@ function shuffle(array) {
   return array
 }
 
-function addStartingQuotes(data) {
-  quotes = data.quotes.map((o => o.quote));
-  quotes = shuffle(quotes);
+class TypingPrompt {
+  #promptInitLength = 600;
+  #promptEl = document.querySelector("#word-box-prompt");
+  #input = "";
+  #promptIndex = 0;
 
-  promptElement = document.querySelector("#word-box-prompt");
+  async init() {
+    await this.#addStartingQuotes()
+    this.#addKeyBoardListener()
+  }
 
-  text = quotes.join('');
-  for (let i = 0; i < 600; i++) {
-    promptElement.innerHTML += `<span>${text[i]}</span>`
+  // add logic just in case prompthLength > text
+  async #addStartingQuotes() {
+    const quotes = await this.#getQuotes();
+    let text = quotes.join('');
+
+    for (let i = 0; i < this.#promptInitLength; i++) {
+      this.#promptEl.innerHTML += `<span>${text[i]}</span>`
+    }
+
+    this.lettersEl =  this.#promptEl.querySelectorAll('span')
+  }  
+
+  #addKeyBoardListener() {
+    document.addEventListener('keydown', (event) => {
+      if (event.key === "Backspace" && this.#input.length) {
+        this.#deleteChar()
+      } 
+      if (event.key.length === 1) {
+          this.#addChar(event)
+      }
+    })
+  }
+
+  async #getQuotes() {
+    if (!this.quotes) {
+      let data = await this.#loadQuotes();
+      let quotes = data.quotes.map((o => o.quote));
+      this.quotes = shuffle(quotes);
+    }
+    return this.quotes;
+  }
+
+  async #loadQuotes() {
+    try {
+      const res = await fetch('quotes.json');
+      const data = await res.json();
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  #addChar(event) {
+    if (event.key === "'") {
+      event.preventDefault();
+    }
+
+    this.#input += event.key;
+    if (this.#input[this.#promptIndex] !== this.lettersEl[this.#promptIndex].innerHTML) {
+      this.lettersEl[this.#promptIndex].style.color = 'red';
+    } else {
+      this.lettersEl[this.#promptIndex].style.color = 'white';
+    }
+    this.#promptIndex += 1;    
+  }
+
+  #deleteChar() {
+    this.#promptIndex = Math.max(0, this.#promptIndex - 1);
+    this.#input = this.#input.slice(0, -1);
+    this.lettersEl[this.#promptIndex].style.color = 'gray';    
   }
 }
 
-loadQuotes().then(data => {
-  addStartingQuotes(data)
-
-  promptElement = document.querySelector("#word-box-prompt");
-  letters = promptElement.querySelectorAll('span')
-
-  input = "";
-  i = 0;
-  document.addEventListener('keydown', (event) => {
-    if (event.key === "Backspace" && input.length) {
-      i = Math.max(0, i - 1);
-      input = input.slice(0, -1);
-      letters[i].style.color = 'gray';
-    } 
-    if (event.key.length === 1) {
-        if (event.key === "'") {
-          event.preventDefault();
-        }
-
-        input += event.key;
-        if (input[i] !== letters[i].innerHTML) {
-          letters[i].style.color = 'red';
-        } else {
-          letters[i].style.color = 'white';
-        }
-        i += 1;
-    }
-  })
-});
-
+let typingPrompt = new TypingPrompt();
+typingPrompt.init()
